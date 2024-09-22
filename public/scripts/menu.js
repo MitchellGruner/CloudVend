4c5716b93cd38763dbc102fd18188d330be10cdb
Mitchell Gruner <mit.gruner@gmail.com>
2024-09-22 12:37:44 -0500

    add click event to hide hamburger menu

--- public/scripts/menu.js
+++ public/scripts/menu.js
@@ -1,6 +1,7 @@
 /* hamburger menu functionality */
 document.addEventListener("DOMContentLoaded", function() {
     const hamburgerMenu = document.querySelector(".hamburger-menu");
+    const menuDiv = document.querySelector(".menu-div");
     const offScreenMenu = document.querySelector(".off-screen-menu");
     const itemsContainer = document.querySelector(".display-items-container-div");
     const menuHeight = document.querySelector(".menu-div").offsetHeight;
@@ -20,6 +21,14 @@ document.addEventListener("DOMContentLoaded", function() {
             itemsContainer.style.opacity = "1";
         } else {
             itemsContainer.style.opacity = "0.5";
+        }
+    });
+
+    document.addEventListener("click", (event) => {
+        if (!hamburgerMenu.contains(event.target) && !menuDiv.contains(event.target) && !offScreenMenu.contains(event.target)) {
+            hamburgerMenu.classList.remove("active");
+            offScreenMenu.classList.remove("active");
+            itemsContainer.style.opacity = "1";
         }
     });
 });
