9ee803fe42bcf4ab1e5d5c69cde7410c15329853
Mitchell Gruner <mit.gruner@gmail.com>
2024-11-05 14:34:11 -0600

    add middleware for checking if profile has been created

--- middleware/index.js
+++ middleware/index.js
@@ -1,12 +1,37 @@
+Profile = require('../models/profile');
+
 // all the middleware goes here
 var middlewareObj = {};
-	
+
 // middleware for determining if user is logged in.
 middlewareObj.isLoggedIn = function(req, res, next){
-	if(req.isAuthenticated()){
-		return next();
-	}
-	res.redirect("/login");	
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");    
+};
+
+// middleware for checking if a profile exists
+middlewareObj.checkProfile = function(req, res, next) {
+    if (req.isAuthenticated()) { // Ensure the user is authenticated
+        Profile.findOne({ user: req.user._id }, (err, foundProfile) => {
+            if (err) {
+                res.locals.profileExists = false;
+            } else {
+                res.locals.profileExists = !!foundProfile;
+            }
+            next();
+        });
+    } else {
+        res.locals.profileExists = false;
+        next();
+    }
+};
+
+// middleware for setting currentUser
+middlewareObj.setCurrentUser = function(req, res, next) {
+    res.locals.currentUser = req.user;
+    next();
 };
 
 module.exports = middlewareObj;
