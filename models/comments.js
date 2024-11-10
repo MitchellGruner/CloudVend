5e0dd9881eea9df11bc5816ffbf6f2f5c36ceacb
Mitchell Gruner <mit.gruner@gmail.com>
2024-11-10 15:28:39 -0600

    add profile information to comments model

--- models/comments.js
+++ models/comments.js
@@ -1,22 +1,27 @@
 var mongoose = require('mongoose');
 
 var commentSchema = new mongoose.Schema({
-	text: String,
-	author: {
-		id: {
-			type: mongoose.Schema.Types.ObjectId,
-			href: "User"
-		},
-		username: String,
-		city: String
-	},
-	profile: {
-		id: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: "Profile"
-		},
-		image: String,
-	},
+    text: String,
+    author: {
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        username: String,
+        city: String,
+        email: String,
+        image: String,
+        profileId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Profile"
+        },
+        profileImage: String,
+        biography: String,
+        backgroundImage: String,
+        facebook: String,
+        instagram: String,
+        other: String
+    }
 });
 
 module.exports = mongoose.model("Comment", commentSchema);
