6da5eb4a4c3a29dc60e1a6e910791e48bca7a57c
Mitchell Gruner <mit.gruner@gmail.com>
2024-11-10 15:30:01 -0600

    add 'Profile' info to associated comment

--- routes/comments.js
+++ routes/comments.js
@@ -17,37 +17,64 @@ router.get("/items/:id/comments/new", middleware.isLoggedIn, (req, res) => {
 
 // this logic will create a comment.
 router.post("/items/:id/comments/new", middleware.isLoggedIn, (req, res) => {
-	Items.findById(req.params.id, (err, items) => {
-		if(err){
-			console.log(err);
-			res.redirect("/items");
-		} else {
-			var text = req.body.text;
-			var author = {
-				id: req.user._id,
-				username: req.user.username,
-				city: req.user.city
-			};
-			var newComment = {text: text, author: author};
-			Comment.create(newComment, (err, comment) => {
-				if(err){
-					console.log(err);
-				} else {
-					comment.author.id = req.user._id;
-					comment.author.username = req.user.username;
-					comment.author.city = req.user.city;
-					comment.save();
-					
-					items.comments.push(comment);
-					items.save();
-					
-					console.log(comment);
-					
-					res.redirect("/items/" + items._id);
-				}
-			});
-		}
-	});
+    Items.findById(req.params.id, (err, items) => {
+        if (err) {
+            console.log(err);
+            res.redirect("/items");
+        } else {
+			
+            // find the profile associated with the user.
+            Profile.findOne({ user: req.user._id }, (err, foundProfile) => {
+                if (err || !foundProfile) {
+                    console.log(err);
+                    res.redirect("/items");
+                } else {
+                    var text = req.body.text;
+                    var author = {
+                        id: req.user._id,
+                        username: req.user.username,
+                        city: req.user.city,
+                        email: req.user.email,
+                        image: req.user.image,
+                        profileId: foundProfile._id,
+                        profileImage: foundProfile.image,
+                        biography: foundProfile.biography,
+                        backgroundImage: foundProfile.backgroundImage,
+                        facebook: foundProfile.facebook,
+                        instagram: foundProfile.instagram,
+                        other: foundProfile.other
+                    };
+                    var newComment = { text: text, author: author };
+                    Comment.create(newComment, (err, comment) => {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            comment.author.id = req.user._id;
+                            comment.author.username = req.user.username;
+                            comment.author.city = req.user.city;
+                            comment.author.email = req.user.email;
+                            comment.author.image = req.user.image;
+                            comment.author.profileId = foundProfile._id;
+                            comment.author.profileImage = foundProfile.image;
+                            comment.author.biography = foundProfile.biography;
+                            comment.author.backgroundImage = foundProfile.backgroundImage;
+                            comment.author.facebook = foundProfile.facebook;
+                            comment.author.instagram = foundProfile.instagram;
+                            comment.author.other = foundProfile.other;
+                            comment.save();
+
+                            items.comments.push(comment);
+                            items.save();
+
+                            console.log(comment);
+
+                            res.redirect("/items/" + items._id);
+                        }
+                    });
+                }
+            });
+        }
+    });
 });
 
 // comment destroy route.
