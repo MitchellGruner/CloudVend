477238e70ae40f2f4c4174c314aa93baf5755984
Mitchell Gruner <mit.gruner@gmail.com>
2024-11-13 15:43:50 -0600

    add secondary paragraph length for 'show' items

--- public/scripts/addApostropheFunctionality.js
+++ public/scripts/addApostropheFunctionality.js
@@ -1,13 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const paragraphs = document.querySelectorAll("main section#display-items.display-items-container div:nth-child(2) div div.container p");
+    function truncateParagraphs(selector, maxLength) {
+        const paragraphs = document.querySelectorAll(selector);
+        paragraphs.forEach(p => {
+            const originalText = p.textContent;
+            if (originalText.length > maxLength) {
+                const truncatedText = originalText.substring(0, maxLength) + '...';
+                p.textContent = truncatedText;
+            }
+        });
+    }
 
-    paragraphs.forEach(p => {
-        const originalText = p.textContent;
-        const maxLength = 200;
-
-        if (originalText.length > maxLength) {
-            const truncatedText = originalText.substring(0, maxLength) + '"...';
-            p.textContent = truncatedText;
-        }
-    });
+    truncateParagraphs("main section#display-items.display-items-container div:nth-child(2) div div.container p.paragraph-limit-short", 200);
+    truncateParagraphs("main section#display-items.display-items-container.show-item div:nth-child(2) div div.container p", 400);
 });
